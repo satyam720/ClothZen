8618b18284a53acaf39d6bb487ef585afafe94f4
satyam1906 <psatyam1618@gmail.com>
2021-08-03 17:54:54 +0530

    New action and Payload defined in cartReducer to handle AddItems

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,11 @@
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import {connect} from 'react-redux';
+import {addItem } from '../../redux/cart/cart.actions';
 
-
-const CollectionItem = ({ id, name, imageUrl, price}) => (
+const CollectionItem = ({  item,addItem}) => {
+    const { name, imageUrl, price} = item;
+    return(
     <div className="collection-item">
         <div 
         className="image"
@@ -13,8 +16,12 @@ const CollectionItem = ({ id, name, imageUrl, price}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
-        <CustomButton inverted> Add to Cart</CustomButton>
+        <CustomButton onClick={() => addItem(item)} inverted> Add to Cart</CustomButton>
     </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: (item) => dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CollectionItem);
