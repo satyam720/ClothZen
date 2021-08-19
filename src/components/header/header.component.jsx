ceac96ed631e9854b54eeb684b769d71f5e2ecf7
satyam1906 <psatyam1618@gmail.com>
2021-08-19 20:57:28 +0530

    updated header and homepage to use styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,29 +8,32 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import {selectCurrentUser} from '../../redux/user/user.selector';
+import {HeaderContainer,LogoContainer, OptionsContainer,OptionDiv,OptionLink} from './header.styles';
+
+
 
 const Header =({currentUser,hidden}) => (
-  <div className="header">
-    <Link className="logo-container" to ="/">
+  <HeaderContainer>
+    <LogoContainer to ="/">
         <Logo className="logo" />
-    </Link>
+    </LogoContainer>
     
-  <div className="options">
-      <Link className="option" to="/shop">SHOP</Link>
-      <Link className="option" to="/shop">CONTACT</Link>
+  <OptionsContainer>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/shop">CONTACT</OptionLink>
       {
         currentUser ?
-        (<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>) :
-        <Link className="option" to ="/signin">SIGN IN</Link>
+        (<OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>) :
+        <OptionLink to ="/signin">SIGN IN</OptionLink>
       }
       <CartIcon />
-  </div>
+  </OptionsContainer>
   {
     hidden ? null : 
     (<CartDropdown />)
   }
   
-  </div>);
+  </HeaderContainer>);
 
 
 const mapStateToProps = createStructuredSelector({
