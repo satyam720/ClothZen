593b6d6b759f9fc6ee2766099f194b67914a3a11
satyam1906 <psatyam1618@gmail.com>
2021-08-20 17:50:31 +0530

    moved shop data to firebase

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.components';
-import {auth,createUserProfileDocument } from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import { setCurrentUser} from "./redux/user/user.actions";
 import { selectCurrentUser } from './redux/user/user.selector';
 
-
+ 
 
 class App extends React.Component {
 
@@ -44,6 +44,7 @@ class App extends React.Component {
     }
     
       setCurrentUser(userAuth);
+      
     
     });
   };
@@ -69,7 +70,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  
 })
 
 const mapDispatchToProps = (dispatch) => ({
